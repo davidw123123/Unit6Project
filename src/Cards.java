89fdff89047d09c379c4d006ce85444b4f98b9e7
davidw123123 <davidwg1210@gmail.com>
2025-01-30 22:53:49 -0500

    Finished part one. Fixed some error in the if statements for some methods

--- src/Cards.java
+++ src/Cards.java
@@ -1,8 +1,4 @@
-import java.util.Arrays;
-
 public class Cards {
-
-
     public int[] duplicates(String[] arr)
     {
         String[] cards = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
@@ -10,9 +6,8 @@ public class Cards {
         for (int i = 0; i < cards.length; i++)
         {
             int count = 0;
-            for (int x = 0; x < arr.length; x++)
-            {
-                if (cards[i].equals(arr[x])) {
+            for (String s : arr) {
+                if (cards[i].equals(s)) {
                     count++;
                 }
             }
@@ -21,4 +16,148 @@ public class Cards {
         return counts;
     }
 
+
+    public int fiveOfAKind(int[] arr)
+    {
+        int count = 0;
+        for (int numbers : arr){
+            if (numbers == 5)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public int fourOfAKind(int[] arr)
+    {
+        int count = 0;
+        for (int numbers : arr){
+            if (numbers == 4)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public int fullHouse(int[] arr) {
+        int countThree = 0;
+        int countTwo = 0;
+
+        for (int numbers : arr) {
+            if (numbers == 3) {
+                countThree++;
+            } else if (numbers == 2) {
+                countTwo++;
+            }
+        }
+
+        if (countThree == 1 && countTwo == 1) {
+            return 1;
+        }
+        return 0;
+    }
+    public int threeOfAKind(int[] arr)
+    {
+        int count = 0;
+        int countOne = 0;
+        for (int numbers : arr){
+            if (numbers == 3)
+            {
+                count++;
+            }
+            if (numbers == 1)
+            {
+                countOne++;
+            }
+        }
+        if (countOne == 0)
+        {
+            count = 0;
+        }
+        return count;
+    }
+    public int twoPair(int[] arr) {
+        int count = 0;
+
+        for (int numbers : arr) {
+            if (numbers == 2) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            return 1;
+        }
+        return 0;
+    }
+    public int onePair(int[] arr)
+    {
+        int count = 0;
+        int countOne = 0;
+        for (int numbers : arr){
+            if (numbers == 1)
+            {
+                countOne ++;
+            }
+            if ( numbers == 2)
+            {
+                count++;
+            }
+        }
+        if (countOne != 3)
+        {
+            count = 0;
+        }
+        return count;
+    }
+    public int highCard(int[] arr)
+    {
+        int count = 0;
+        for (int numbers : arr){
+            if (numbers == 1)
+            {
+                count ++;
+            }
+        }
+        if (count != 5){
+            count = 0;
+        } else count = 1;
+        return count;
+    }
+    public String hands(String [] arr)
+    {
+        int five = 0;
+        int four = 0;
+        int full = 0;
+        int three = 0;
+        int two = 0;
+        int one = 0;
+        int high = 0;
+        for (String hand : arr) {
+            String[] temp = hand.split(",");
+            int[] counts = duplicates(temp);
+
+            if (fiveOfAKind(counts) == 1) {
+                five++;
+            } else if (fourOfAKind(counts) == 1) {
+                four++;
+            } else if (fullHouse(counts) == 1) {
+                full++;
+            } else if (threeOfAKind(counts) == 1) {
+                three++;
+            } else if (twoPair(counts) == 1) {
+                two++;
+            } else if (onePair(counts) == 1) {
+                one++;
+            } else {
+                high++;
+            }
+        }
+        return "Number of five of a kind hands: " + five + "\n" + "Number of full house hands: " + full + "\n" + "Number of four of a kind hands: " + four + "\n" + "Number of three of a kind hands: " + three + "\n" + "Number of two pair hands: " + two + "\n" + "Number of one pair hands: " + one + "\n" + "Number of high card hands: " + high;
+
+
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 
+
+
 public class Main {
     public static void main(String[] args) {
         Cards cards = new Cards();
 
+
         // step 1: create a file object
         File f = new File("src/input_file");
+
+
 
 
         String fileData = "";
@@ -22,25 +27,18 @@ public class Main {
             // a String array where every item in the array is a line from the file
             String[] fileArray = fileData.split("\n");
             String[] stringNumbers = new String[fileArray.length];
+
             for (int i=0; i < fileArray.length; i++) {
                 // removes |
                 stringNumbers[i] = Arrays.toString(fileArray[i].split(" "));
             }
-            System.out.println(Arrays.toString(stringNumbers));
+
             for (int i=0; i < fileArray.length; i++) {
                 // split by space, now we have a list of String numbers
                 stringNumbers[i] =  fileArray[i].substring(0,  fileArray[i].indexOf("|"));
             }
-            System.out.println(Arrays.toString(stringNumbers));
-            String [] test = stringNumbers[0].split(",");
-            System.out.println(Arrays.toString(test));
-            System.out.println(Arrays.toString(cards.duplicates(test)));
-//            String[] stringNumber = new String[stringNumbers.length];
-//            for (int i=0; i < stringNumbers.length; i++) {
-//                stringNumber[i] = Arrays.toString(stringNumbers[i].split(" "));
-//            }
-//            System.out.println(Arrays.toString(stringNumber));
-//            System.out.println(stringNumber[0]);
+            //part one
+            System.out.println(cards.hands(stringNumbers));
         }
         catch (FileNotFoundException fe) {
             System.out.println("File was not found");
@@ -48,6 +46,7 @@ public class Main {
         }
 
 
+
+
     }
 }
-
