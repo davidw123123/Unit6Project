0a4d0c77fd48b5d9a1f5872cfca4fbefef8fd37b
BT_2S14_31 <gabriellej34@nycstudents.net>
2025-02-04 10:54:55 -0500

    worked on methods for part two

--- src/Cards.java
+++ src/Cards.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -9,6 +10,7 @@ public class Cards {
     ArrayList<String> twoPair = new ArrayList<>();
     ArrayList<String> onePair = new ArrayList<>();
     ArrayList<String> high = new ArrayList<>();
+    ArrayList<String> all = new ArrayList<>();
     ArrayList<Integer> fiveCardBidValue = new ArrayList<>();
     ArrayList<Integer> fourCardBidValue = new ArrayList<>();
     ArrayList<Integer> fullHouseBidValue = new ArrayList<>();
@@ -16,13 +18,12 @@ public class Cards {
     ArrayList<Integer> twoPairBidValue = new ArrayList<>();
     ArrayList<Integer> onePairBidValue = new ArrayList<>();
     ArrayList<Integer> highBidValue = new ArrayList<>();
-    int [] bidValue = new int[50];
-    public int[] duplicates(String[] arr)
-    {
+    int[] bidValue = new int[50];
+
+    public int[] duplicates(String[] arr) {
         String[] cards = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
         int[] counts = new int[13];
-        for (int i = 0; i < cards.length; i++)
-        {
+        for (int i = 0; i < cards.length; i++) {
             int count = 0;
             for (String s : arr) {
                 if (cards[i].equals(s)) {
@@ -34,12 +35,10 @@ public class Cards {
         return counts;
     }
 
-    public int fiveOfAKind(int[] arr)
-    {
+    public int fiveOfAKind(int[] arr) {
         int count = 0;
-        for (int numbers : arr){
-            if (numbers == 5)
-            {
+        for (int numbers : arr) {
+            if (numbers == 5) {
                 count++;
             }
         }
@@ -47,17 +46,16 @@ public class Cards {
     }
 
 
-    public int fourOfAKind(int[] arr)
-    {
+    public int fourOfAKind(int[] arr) {
         int count = 0;
-        for (int numbers : arr){
-            if (numbers == 4)
-            {
+        for (int numbers : arr) {
+            if (numbers == 4) {
                 count++;
             }
         }
         return count;
     }
+
     public int fullHouse(int[] arr) {
         int countThree = 0;
         int countTwo = 0;
@@ -75,26 +73,24 @@ public class Cards {
         }
         return 0;
     }
-    public int threeOfAKind(int[] arr)
-    {
+
+    public int threeOfAKind(int[] arr) {
         int count = 0;
         int countOne = 0;
-        for (int numbers : arr){
-            if (numbers == 3)
-            {
+        for (int numbers : arr) {
+            if (numbers == 3) {
                 count++;
             }
-            if (numbers == 1)
-            {
+            if (numbers == 1) {
                 countOne++;
             }
         }
-        if (countOne == 0)
-        {
+        if (countOne == 0) {
             count = 0;
         }
         return count;
     }
+
     public int twoPair(int[] arr) {
         int count = 0;
 
@@ -109,42 +105,38 @@ public class Cards {
         }
         return 0;
     }
-    public int onePair(int[] arr)
-    {
+
+    public int onePair(int[] arr) {
         int count = 0;
         int countOne = 0;
-        for (int numbers : arr){
-            if (numbers == 1)
-            {
-                countOne ++;
+        for (int numbers : arr) {
+            if (numbers == 1) {
+                countOne++;
             }
-            if ( numbers == 2)
-            {
+            if (numbers == 2) {
                 count++;
             }
         }
-        if (countOne != 3)
-        {
+        if (countOne != 3) {
             count = 0;
         }
         return count;
     }
-    public int highCard(int[] arr)
-    {
+
+    public int highCard(int[] arr) {
         int count = 0;
-        for (int numbers : arr){
-            if (numbers == 1)
-            {
-                count ++;
+        for (int numbers : arr) {
+            if (numbers == 1) {
+                count++;
             }
         }
-        if (count != 5){
+        if (count != 5) {
             count = 0;
         } else count = 1;
         return count;
     }
-    public String hands(String [] arr)
-    {
+
+    public String hands(String[] arr) {
         int five = 0;
         int four = 0;
         int full = 0;
@@ -176,12 +168,11 @@ public class Cards {
 
 
     }
-    public void setBidValue(String str)
-    {
+
+    public void setBidValue(String str) {
         String[] temp = str.split("\n");
-        for (int i = 0; i < temp.length; i ++)
-        {
-            bidValue[i] = Integer.parseInt(temp[i].substring(temp[i].indexOf("|")+1));
+        for (int i = 0; i < temp.length; i++) {
+            bidValue[i] = Integer.parseInt(temp[i].substring(temp[i].indexOf("|") + 1));
         }
     }
 
@@ -206,10 +197,9 @@ public class Cards {
     }
 
     //part 2
-    public void setHandArrayList(String str)
-    {
+    public void setHandArrayList(String str) {
         String[] stringNumbers = setHand(str);
-        for (int i = 0; i < stringNumbers.length; i ++) {
+        for (int i = 0; i < stringNumbers.length; i++) {
             if (fiveOfAKind(singleDuplicates(stringNumbers[i])) == 1) {
                 fiveCard.add(stringNumbers[i]);
                 fiveCardBidValue.add(bidValue[i]);
@@ -233,13 +223,12 @@ public class Cards {
             highBidValue.add(bidValue[i]);
         }
     }
-    public int[] singleDuplicates(String singleDeck)
-    {
+
+    public int[] singleDuplicates(String singleDeck) {
         String[] cards = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
         int[] counts = new int[13];
         String[] temp = singleDeck.split(",");
-        for (int i = 0; i < cards.length; i++)
-        {
+        for (int i = 0; i < cards.length; i++) {
             int count = 0;
             for (String s : temp) {
                 if (cards[i].equals(s)) {
@@ -251,6 +240,75 @@ public class Cards {
         return counts;
     }
 
+    public String strongerDeck(String deck1, String deck2)
+    {
+        String[] tempDeck1 = deck1.split(",");
+        String[] tempDeck2 = deck2.split(",");
+        for (int i = 0; i < tempDeck1.length; i++)
+        {
+            for (int j = 0; j < tempDeck2.length; j ++)
+            {
+                if (cardStrength(tempDeck1[i]) > cardStrength(tempDeck2[j]))
+                {
+                    return deck1;
+                } else if (cardStrength(tempDeck1[i]) < cardStrength(tempDeck2[j]))
+                {
+                    return deck2;
+                }
+            }
+        }
+        return "Same";
+    }
+    public void sortHand(ArrayList list, ArrayList bid)
+    {
+        for (int i = 0; i < list.size(); i ++) {
+            for (int j = 1; j < list.size(); j++) {
+                String stronger = strongerDeck(list.get(i).toString(), list.get(j).toString());
+                if (stronger.equals(list.get(j).toString()))
+                {
+                    String temp = list.get(i).toString();
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+
+                    int tempBid = Integer.parseInt(bid.get(i).toString());
+                    bid.set(i, bid.get(j));
+                    bid.set(j, tempBid);
+                }
+            }
+        }
+    }
+//    public String calculateBidValue()
+//    {
+//        int[] bidAmount = new int[50];
+//        for (int i = 0; i < high.size(); i ++)
+//        {
+//
+//        }
+//    }
+//    public void allSort()
+//    {
+//        sortHand(high, high);
+//    }
+    public int cardStrength(String card)
+    {
+        if (card.equals("Ace"))
+        {
+            return 14;
+        }
+        if (card.equals("King"))
+        {
+            return 13;
+        }
+        if (card.equals("Queen"))
+        {
+            return 12;
+        }
+        if (card.equals("Jack"))
+        {
+            return 11;
+        }
+        return Integer.parseInt(card);
+    }
     public ArrayList<String> getFiveCard() {
         return fiveCard;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
@@ -22,14 +23,16 @@ public class Main {
 
             // a String array where every item in the array is a line from the file
             String[] stringNumbers = cards.setHand(fileData);
+            System.out.println(cards.strongerDeck(stringNumbers[0], stringNumbers[1]));
             System.out.println(Arrays.toString(stringNumbers));
             //part one
             System.out.println(cards.hands(stringNumbers));
             //part two
             cards.setBidValue(fileData);
             cards.setHandArrayList(fileData);
-            System.out.println(cards.getFourCard());
-            System.out.println(cards.getBidValue());
+            cards.sortHand(cards.twoPair, cards.twoPairBidValue);
+            System.out.println(cards.twoPair);
+            System.out.println(Arrays.toString(cards.bidValue));
         }
         catch (FileNotFoundException fe) {
             System.out.println("File was not found");
